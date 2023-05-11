0e4a0a0a387b80c5bc1c3230cc309fea4c65dcd2
KolenkoGri <130161094+KolenkoGri@users.noreply.github.com>
2023-05-11 16:35:43 +0400

    Add files via upload

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 
 (() => {
-  const FIGURES_ENG = ['rock', 'scissors', 'paper'];
-  const FIGURES_RUS = ['камень', 'ножницы', 'бумага'];
+  const FIGURES_ENG = ["rock", "scissors", "paper"];
+  const FIGURES_RUS = ["камень", "ножницы", "бумага"];
 
   const getRandomIntInclusive = (min, max) => {
     min = Math.ceil(min);
@@ -15,27 +15,30 @@
       player: 0,
       computer: 0,
     };
-    const lang = language === 'EN' || language === 'ENG' ?
-    FIGURES_ENG : FIGURES_RUS;
+    const lang =
+      language === "EN" || language === "ENG" ? FIGURES_ENG : FIGURES_RUS;
 
     const getFigure = () => {
-      const exitExpression = lang === FIGURES_ENG ?
-      'Are you sure you want to get out?' : 'Точно ли Вы хотите выйти?';
+      const exitExpression =
+        lang === FIGURES_ENG
+          ? "Are you sure you want to get out?"
+          : "Точно ли Вы хотите выйти?";
       return exitExpression;
     };
 
-    const compWord = lang === FIGURES_ENG ? 'Computer' : 'Компьютер';
-    const you = lang === FIGURES_ENG ? 'You' : 'Вы';
-    const winner = lang === FIGURES_ENG ? 'win' : 'выиграл';
-    const draw = lang === FIGURES_ENG ? 'draw' : 'Ничья';
-    const user = lang === FIGURES_ENG ? 'player' : 'Игрок';
-    const resultat = lang === FIGURES_ENG ? 'Result' : 'Результат';
+    const compWord = lang === FIGURES_ENG ? "Computer" : "Компьютер";
+    const you = lang === FIGURES_ENG ? "You" : "Вы";
+    const winner = lang === FIGURES_ENG ? "win" : "выиграл";
+    const draw = lang === FIGURES_ENG ? "draw" : "Ничья";
+    const user = lang === FIGURES_ENG ? "player" : "Игрок";
+    const resultat = lang === FIGURES_ENG ? "Result" : "Результат";
 
     return function start() {
       const computer = getRandomIntInclusive(0, 2);
       console.log(computer);
-      const answer = prompt(`${lang}?`).toLowerCase();
+      let answer = prompt(`${lang}?`);
       if (answer) {
+        answer = answer.toLowerCase();
         const arr = [...lang];
         const compAnswer = arr.slice(computer, computer + 1).toString();
         if (answer[0] === compAnswer[0]) {
@@ -45,7 +48,7 @@
                         ${draw}`);
           return start();
         }
-        if (answer[0] === 'к' || answer[0] === 'r') {
+        if (answer[0] === "к" || answer[0] === "r") {
           if (compAnswer === lang[2]) {
             result.computer += 1;
             alert(`
@@ -61,7 +64,7 @@
                 ${user} ${winner}`);
           }
         }
-        if (answer[0] === 'н' || answer[0] === 's') {
+        if (answer[0] === "н" || answer[0] === "s") {
           if (compAnswer === lang[0]) {
             result.computer += 1;
             alert(`
@@ -77,7 +80,7 @@
                 ${user} ${winner}`);
           }
         }
-        if (answer[0] === 'б' || answer[0] === 'p') {
+        if (answer[0] === "б" || answer[0] === "p") {
           if (compAnswer === lang[1]) {
             result.computer += 1;
             alert(`
